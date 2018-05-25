36be440f6f7338f9896112dd34c8fcf346d6507e
saltowl <vladislava-shakirova@mail.ru>
2018-05-25 21:51:33 +0300

    Add PrintResult()

--- Runge-Kutta Methods/Runge_Kutta_Methods.py
+++ Runge-Kutta Methods/Runge_Kutta_Methods.py
@@ -72,6 +72,26 @@ class DifferentialEquation:
         plt.legend()
         plt.show()
 
+
+    def PrintResult(self):
+        RK1 = self.EulersMethod()
+        RK2 = self.SecondOrderMethod()
+        RK4 = self.RK4()
+        real = self.RealValue()
+
+        print("_____________________________________________________________________________________")
+        print('|     | Method Runge-Kutta | Method Runge-Kutta | Method Runge-Kutta |               |')
+        print('|  X  |    first order     |     second order   |     fourth order   |   Real Value  |')
+        print("|_____|____________________|____________________|____________________|_______________|")
+
+        for i in range(len(self.__x)):
+            print('|{0:^5}|{1:^20}|{2:^20}|{3:^20}|{4:^15}|'
+                  .format(round(self.__x[i], 2), round(RK1[i], 9), round(RK2[i], 9), 
+                          round(RK4[i], 9), round(real[i][0], 9)))
+        
+        print('|_____|____________________|____________________|____________________|_______________|')
+
+
 def Main():
     a = 0
     b = 1
@@ -79,6 +99,7 @@ def Main():
     y0 = 0
     ude = DifferentialEquation(a, b, h, y0)
     ude.Graph()
+    ude.PrintResult()
     pass
 
 Main()
